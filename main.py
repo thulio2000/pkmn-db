8181585f468addbe6b80d0ea706f44635c95d72a
Túlio <lucastuliolima@gmail.com>
2023-03-30 17:59:42 -0300

    "Def sqlConnect and create_database functions"

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("Hello pkmn")
+import mysql.connector
+
+
+def sqlConnect():
+    return mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="q0fmUEe16wNq)~2G",
+        database="PokemonPy",
+        auth_plugin="mysql_native_password"
+    )
+
+
+def create_database():
+    database = sqlConnect()
+    db_cursor = database.cursor()
+    db_cursor.execute("CREATE TABLE IF NOT EXISTS dragon_pokemon (id int AUTO_INCREMENT PRIMARY KEY, Pokemon VARCHAR(255), FastMove VARCHAR(255), ChargeMove VARCHAR(255), DPS float, TDO float, Total float)")
+    db_cursor.execute("SELECT * FROM dragon_pokemon")
