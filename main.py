7a6cb76ff124e7115c6c5b84fccaf360b8c47d82
Túlio <lucastuliolima@gmail.com>
2023-03-30 18:05:18 -0300

    "Beginning of add_pokemon function"

--- main.py
+++ main.py
@@ -16,3 +16,11 @@ def create_database():
     db_cursor = database.cursor()
     db_cursor.execute("CREATE TABLE IF NOT EXISTS dragon_pokemon (id int AUTO_INCREMENT PRIMARY KEY, Pokemon VARCHAR(255), FastMove VARCHAR(255), ChargeMove VARCHAR(255), DPS float, TDO float, Total float)")
     db_cursor.execute("SELECT * FROM dragon_pokemon")
+
+
+def add_pokemon():
+    database = sqlConnect()
+    db_cursor = database.cursor()
+    Pokemon = input("Enter Pokemon: ")
+    FastMove = input("Enter Fast Move: ")
+    ChargeMove = input("Enter Charge Move: ")
