9ca1e9f8be088ab35d09665dc7118c9652ca0b5d
Túlio <lucastuliolima@gmail.com>
2023-04-03 16:39:57 -0300

    "Commenting each function"

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import mysql.connector
 
 
-def sqlConnect():
+def sqlConnect():  # connects to the db
     return mysql.connector.connect(
         host="localhost",
         user="root",
@@ -11,13 +11,14 @@ def sqlConnect():
     )
 
 
-def create_database():
+def create_database():  # creates dragon_pokemon table with 6 columns
     database = sqlConnect()
     db_cursor = database.cursor()
     db_cursor.execute("CREATE TABLE IF NOT EXISTS dragon_pokemon (id int AUTO_INCREMENT PRIMARY KEY, Pokemon VARCHAR(255), FastMove VARCHAR(255), ChargeMove VARCHAR(255), DPS float, TDO float, Total float)")
     db_cursor.execute("SELECT * FROM dragon_pokemon")
 
 
+# prompts the user to enter values of each column created such as Pokemon name, moves, dps etc.
 def add_pokemon():
     database = sqlConnect()
     db_cursor = database.cursor()
@@ -33,7 +34,7 @@ def add_pokemon():
     print(db_cursor.rowcount, "record inserted")
 
 
-def view_pokemon():
+def view_pokemon():  # retrieves all records from dragon_pokemon using SELECT and fetchall()
     database = sqlConnect()
     db_cursor = database.cursor()
     db_cursor.execute("SELECT * FROM dragon_pokemon")
@@ -42,8 +43,8 @@ def view_pokemon():
         print(dragonpokemon)
 
 
-def update_pokemon():
-    database = sqlConnect
+def update_pokemon():  # prompts the user to enter new values to each column from a specified id
+    database = sqlConnect()
     db_cursor = database.cursor()
     Pokemon = input("Enter pokemon: ")
     FastMove = input("Enter Fast Move: ")
@@ -57,16 +58,16 @@ def update_pokemon():
     print(db_cursor.rowcount, "record(s) affected")
 
 
-def delete_pokemon():
+def delete_pokemon():  # delete a pokemon record from the table using inserting it's id
     database = sqlConnect()
     db_cursor = database.cursor()
     id = input("Enter id: ")
-    db_cursor.execute("DELETE FROM dragon_pokemon WHERE id = %s", (id))
+    db_cursor.execute("DELETE FROM dragon_pokemon WHERE id = %s", (id,))
     database.commit()
     print(db_cursor.rowcount, "record(s) deleted")
 
 
-def main():
+def main():  # calls create database and display a menu of options for the user to choose whether he/she wants to add, view, update, delete or exit
     create_database()
     while True:
         print("1. Add pokemon\n2. View pokemon\n3. Update pokemon \n4. Delete pokemon\n5. Exit")
@@ -85,5 +86,5 @@ def main():
             print("Invalid choice")
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":  # main is only executed if the script is run directly
     main()
