71b89654cfc63d661cb322d7f67a85eab4f68cb9
Túlio <lucastuliolima@gmail.com>
2023-04-03 13:30:13 -0300

    "View and update functions, def main"

--- main.py
+++ main.py
@@ -33,6 +33,30 @@ def add_pokemon():
     print(db_cursor.rowcount, "record(s) affected")
 
 
+def view_pokemon():
+    database = sqlConnect()
+    db_cursor = database.cursor()
+    db_cursor.execute("SELECT * FROM dragon_pokemon")
+    dragonpokemons = db_cursor.fetchall()
+    for dragonpokemon in dragonpokemons:
+        print(dragonpokemon)
+
+
+def update_pokemon():
+    database = sqlConnect
+    db_cursor = database.cursor()
+    Pokemon = input("Enter pokemon: ")
+    FastMove = input("Enter Fast Move: ")
+    ChargeMove = input("Enter Charge Move: ")
+    DPS = input("Enter DPS: ")
+    TDO = input("Enter TDO: ")
+    Total = input("Enter Total: ")
+    db_cursor.execute("UPDATE dragon_pokemon SET Pokemon = %s, FastMove = %s, ChargeMove = %s, DPS = %s, TDO = %s, Total = %s WHERE id = %s",
+                      (Pokemon, FastMove, ChargeMove, DPS, TDO, Total, id))
+    database.commit()
+    print(db_cursor.rowcount, "record(s) affected")
+
+
 def delete_pokemon():
     database = sqlConnect()
     db_cursor = database.cursor()
@@ -40,3 +64,26 @@ def delete_pokemon():
     db_cursor.execute("DELETE FROM dragon_pokemon WHERE id = %s", (id))
     database.commit()
     print(db_cursor.rowcount, "record(s) deleted")
+
+
+def main():
+    create_database()
+    while True:
+        print("1. Add pokemon\n2. View pokemon\n3. Update pokemon \n4. Delete pokemon\n5. Exit")
+        choice = input("Enter your choice: ")
+        if choice == "1":
+            add_pokemon()
+        elif choice == "2":
+            view_pokemon()
+        elif choice == "3":
+            update_pokemon()
+        elif choice == "4":
+            delete_pokemon()
+        elif choice == "5":
+            break
+        else:
+            print("Invalid choice")
+
+
+if __name__ == "__main__":
+    main()
