06e1b4ca7c794709561fb3d78b488f998ea10381
Túlio <lucastuliolima@gmail.com>
2023-04-30 15:34:00 -0300

    "Docker, .env missing keys"

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import psycopg2
+from decouple import config
+import time
+
+print("Starting...")
+time.sleep(10)
 try:
     conn = psycopg2.connect(
-        database="postgres",
-        user="postgres",
-        password="postgres",
-        host="postgres_db"
+        dbname=config("POSTGRES_DB", default="my_database"),
+        user=config("POSTGRES_USER", default="postgres_user"),
+        password=config("POSTGRES_PASSWORD", default="postgres_password"),
+        host=config("POSTGRES_HOST", default="postgres_db")
     )
     print("Connection working!")
 
